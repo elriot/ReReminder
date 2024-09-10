16c2ddd5bf67f9a4cf8e6e6e9dd37a1cf115cde6
Soopin Kim <spkim8901@gmail.com>
2024-09-09 22:12:40 -0700

    initial commit

---
+++ ReReminder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ReReminder
+//
+//  Created by SOOPIN KIM on 2024-09-09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ReReminder/ReReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReReminderApp.swift
+//  ReReminder
+//
+//  Created by SOOPIN KIM on 2024-09-09.
+//
+
+import SwiftUI
+
+@main
+struct ReReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
