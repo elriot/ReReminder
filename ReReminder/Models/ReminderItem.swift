593be6973bbf9c0e974318ce8d295c7ed0fe11c5
Soopin Kim <spkim8901@gmail.com>
2024-09-10 10:27:12 -0700

    Add Term enum

---
+++ ReReminder/Models/ReminderItem.swift
@@ -0,0 +1,15 @@
+//
+//  ReminderItem.swift
+//  ReReminder
+//
+//  Created by SOOPIN KIM on 2024-09-10.
+//
+
+import Foundation
+
+struct ReminderItem: Hashable {
+    let title: String
+    let dDay: String
+    let lastdate: Date
+    let term: String
+}
