44ae42a699fb25088abb2bee18539eac8b4239de
Soopin Kim <spkim8901@gmail.com>
2024-09-12 10:07:50 -0700

    refactor calculator function

--- ReReminder/Models/ReminderItem.swift
+++ ReReminder/Models/ReminderItem.swift
@@ -10,7 +10,7 @@ import Foundation
 struct ReminderItem: Hashable, Identifiable {
     var id: UUID
     var title: String
-    var dDay: String
+    var dDay: Int // if it's 5 it means nextAlertDate - 5 same with "D-5"
     var referenceDate: Date
     var nextAlertDate: Date
     var term: Term
@@ -19,57 +19,63 @@ struct ReminderItem: Hashable, Identifiable {
     var updateDate: Date
     
     init(title: String, referenceDate: Date, term: Term, description: String) {
-        self.id = UUID() // Generate a new UUID
+        self.id = UUID()
         self.title = title
         self.referenceDate = referenceDate
         self.term = term
         self.description = description
-        
-        self.dDay = "D-0"
-        self.nextAlertDate = calculateNextAlertDate()
-        self.valid = true // Default value
-        self.updateDate = Date() // Set to the current date
+        self.valid = true
+        self.updateDate = Date()
+        self.dDay = 0
+        self.nextAlertDate = updateDate
     }
-    
-    func calculateNextAlertDate() -> Date {
-        let today: Date = Date()
-        let calendar = Calendar.current
-        var updateDate = referenceDate
-        
-        switch term {
-        case .once:
-            updateDate = referenceDate
-        case .weekly:
-            while(nextAlertDate < today){
-                updateDate = calendar.date(byAdding: .day, value: 7, to: self.nextAlertDate) ?? referenceDate
-            }
-        case .biWeekly:
-            while(nextAlertDate < today){
-                updateDate = calendar.date(byAdding: .day, value: 14, to: self.nextAlertDate) ?? referenceDate
-            }
-        case .monthly:
-            while(nextAlertDate < today){
-                updateDate = calendar.date(byAdding: .month, value: 1, to: self.nextAlertDate) ?? referenceDate
-            }
-        case .biMonthly:
-            while(nextAlertDate < today){
-                updateDate = calendar.date(byAdding: .month, value: 2, to: self.nextAlertDate) ?? referenceDate
-            }
-        case .everyFirstDay:
-            while nextAlertDate < today {
-                if let nextMonth = calendar.date(byAdding: .month, value: 1, to: nextAlertDate) {
-                    updateDate = calendar.date(from: calendar.dateComponents([.year, .month], from: nextMonth)) ?? nextMonth
-                }
-            }
-        case .everyLastDay:
-            while nextAlertDate < today {
-                if let nextMonth = calendar.date(byAdding: .month, value: 1, to: nextAlertDate),
-                   let lastDay = calendar.date(byAdding: .day, value: -1, to: calendar.date(from: calendar.dateComponents([.year, .month], from: nextMonth))!) {
-                    updateDate = lastDay
-                }
-            }
-        }
-        
-        return updateDate;
-    }
+//    
+//    private func calculateNextAlertDate(date: Date, term: Term) -> Date {
+//        let today = Date()
+//        let calendar = Calendar.current
+//        var nextDate = date
+//        
+//        switch term {
+//        case .once:
+//            return nextDate
+//        case .weekly:
+//            while nextDate < today {
+//                nextDate = calendar.date(byAdding: .day, value: 7, to: nextDate) ?? date
+//            }
+//        case .biWeekly:
+//            while nextDate < today {
+//                nextDate = calendar.date(byAdding: .day, value: 14, to: nextDate) ?? date
+//            }
+//        case .monthly:
+//            while nextDate < today {
+//                nextDate = calendar.date(byAdding: .month, value: 1, to: nextDate) ?? date
+//            }
+//        case .biMonthly:
+//            while nextDate < today {
+//                nextDate = calendar.date(byAdding: .month, value: 2, to: nextDate) ?? date
+//            }
+//        case .everyFirstDay:
+//            while nextDate < today {
+//                if let nextMonth = calendar.date(byAdding: .month, value: 1, to: nextDate) {
+//                    nextDate = calendar.date(from: calendar.dateComponents([.year, .month], from: nextMonth)) ?? nextMonth
+//                }
+//            }
+//        case .everyLastDay:
+//            while nextDate < today {
+//                if let nextMonth = calendar.date(byAdding: .month, value: 1, to: nextDate),
+//                   let lastDay = calendar.date(byAdding: .day, value: -1, to: calendar.date(from: calendar.dateComponents([.year, .month], from: nextMonth))!) {
+//                    nextDate = lastDay
+//                }
+//            }
+//        }
+//        
+//        return nextDate
+//    }
+//    
+//    private func calculateDDay(from nextDate: Date) -> String {
+//        let calendar = Calendar.current
+//        let components = calendar.dateComponents([.day], from: Date(), to: nextDate)
+//        guard let days = components.day else { return "D-0" }
+//        return days >= 0 ? "D-\(days)" : "D+0"
+//    }
 }
